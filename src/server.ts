fbdde047e3a1c7b1954e0585aa509fa2efedc891
Shahid1993 <shahid.bhat1994@gmail.com>
2020-04-04 01:54:36 +0530

    feat: Add endpoint for image filtering
    Add an endpoint in `./src/server.ts` which uses query parameter to download an image from a public URL,
    filter the image, and return the result.

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
@@ -13,27 +14,36 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+  /*
+  GET /filteredimage?image_url={{URL}}
+  endpoint to filter an image from a public url.
+  QUERY PARAMATERS
+      image_url: URL of a publicly accessible image
+  RETURNS
+    the filtered image file
+  */
+  app.get('/filteredimage',
+    async( req: Request, res: Response ) => {
+      const img_url = req.query.image_url;
 
-  /**************************************************************************** */
+      if (!img_url){
+        return res.status(400).send("image_url param is required");
+      }
 
-  //! END @TODO1
+      await filterImageFromURL(img_url)
+      .then((filtered_img_path: string) => {
+        return res.status(200).sendFile(filtered_img_path, () => { 
+          deleteLocalFiles([filtered_img_path]); 
+        });
+      });
+    } 
+  );
+
+  
   
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
+  app.get( "/", async ( req: Request, res: Response ) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
   
